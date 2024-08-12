ab874d86f246a265704ffd95b08e5f6af22860f2
Gu11e <guillegomser@gmail.com>
2024-08-12 23:28:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,115 @@
+let contBox = "0"
+
+function printBox() {
+ document.getElementById(
+   'numberBox').innerText =
+  contBox;
+}
+
+
+function click1() {
+ if (contBox === "0") {
+  contBox = "1"
+  printBox()
+ } else {
+  contBox = contBox + "1"
+  printBox()
+ }
+}
+
+function click2() {
+ if (contBox === "0") {
+  contBox = "2"
+  printBox()
+ } else {
+  contBox = contBox + "2"
+  printBox()
+ }
+}
+
+function click3() {
+ if (contBox === "0") {
+  contBox = "3"
+  printBox()
+ } else {
+  contBox = contBox + "3"
+  printBox()
+ }
+}
+
+function click4() {
+ if (contBox === "0") {
+  contBox = "4"
+  printBox()
+ } else {
+  contBox = contBox + "4"
+  printBox()
+ }
+}
+
+function click5() {
+ if (contBox === "0") {
+  contBox = "5"
+  printBox()
+ } else {
+  contBox = contBox + "5"
+  printBox()
+ }
+}
+
+function click6() {
+ if (contBox === "0") {
+  contBox = "6"
+  printBox()
+ } else {
+  contBox = contBox + "6"
+  printBox()
+ }
+}
+
+function click7() {
+ if (contBox === "0") {
+  contBox = "7"
+  printBox()
+ } else {
+  contBox = contBox + "7"
+  printBox()
+ }
+}
+
+function click8() {
+ if (contBox === "0") {
+  contBox = "8"
+  printBox()
+ } else {
+  contBox = contBox + "8"
+  printBox()
+ }
+}
+
+function click9() {
+ if (contBox === "0") {
+  contBox = "9"
+  printBox()
+ } else {
+  contBox = contBox + "9"
+  printBox()
+ }
+}
+
+function click0() {
+ if (contBox === "0") {
+  contBox = "0"
+  printBox()
+ } else {
+  contBox = contBox + "0"
+  printBox()
+ }
+}
+
+function clickAC() {
+ contBox = "0"
+ printBox()
+}
+
 
